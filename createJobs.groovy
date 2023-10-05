5dd2ea058ba987c21333fef6a3533b143bf2e887
BoPang <1275189619@qq.com>
2023-10-05 14:44:05 +0800

    jenkins-0-pangbo

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,8 +1,14 @@
-pipelineJob('pipelineJob') {
+pipelineJob('oasis-job') {
     definition {
-        cps {
-            script(readFileFromWorkspace('pipelineJob.groovy'))
-            sandbox()
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/BoPang123/NUS_DMSS_PracticeModule_Team3.git'
+                    }
+                    branch 'master'
+                }
+            }
         }
     }
 }
